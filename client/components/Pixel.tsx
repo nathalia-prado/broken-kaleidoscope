e6dd2c70cd4f78240c42bb3dc55d879d63afdcf9
Nathalia Prado <nathalia_898@hotmail.com>
2023-08-15 11:45:29 +1200

    storing the color in component state

--- client/components/Pixel.tsx
+++ client/components/Pixel.tsx
@@ -1,10 +1,13 @@
+import { useState } from 'react'
+
 function Pixel() {
+    const [color, setColor] = useState('cornflowerblue')
     return (
         <div style={{
             fontFamily: 'Times New Roman',
             height: '50px',
             width: '50px',
-            backgroundColor: 'cornflowerblue', 
+            backgroundColor: color, 
         }} ></div>
     )
 }
