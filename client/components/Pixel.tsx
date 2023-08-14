9e6c54c81a8ee6ee8e298b67c179efb6889fa8d6
Nathalia Prado <nathalia_898@hotmail.com>
2023-08-15 11:52:30 +1200

    adding random color to the component

--- client/components/Pixel.tsx
+++ client/components/Pixel.tsx
@@ -1,7 +1,11 @@
 import { useState } from 'react'
 
 function Pixel() {
-    const [color, setColor] = useState('cornflowerblue')
+    const getRandomColor = () =>
+        `#${Math.floor(Math.random() * 0x1000000)
+            .toString(16)
+            .padStart(6, '0')}`
+    const [color, setColor] = useState(getRandomColor)        
     return (
         <div style={{
             fontFamily: 'Times New Roman',
