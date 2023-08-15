42b6f714177a7626b5e8be38644638ca4dc644bb
Nathalia Prado <nathalia_898@hotmail.com>
2023-08-15 12:44:50 +1200

    adding state to handle colors

--- client/components/Pixel.tsx
+++ client/components/Pixel.tsx
@@ -1,18 +1,26 @@
 import { useState } from 'react'
 
 function Pixel() {
+
     const getRandomColor = () =>
         `#${Math.floor(Math.random() * 0x1000000)
             .toString(16)
             .padStart(6, '0')}`
-    const [color, setColor] = useState(getRandomColor)        
+
+    const [color, setColor] = useState(getRandomColor)
+
+    const handleClick = () => {
+        setColor(getRandomColor);
+    }
+
     return (
-        <div style={{
+        <button style={{
             fontFamily: 'Times New Roman',
             height: '50px',
             width: '50px',
             backgroundColor: color, 
-        }} ></div>
+        }} onClick={handleClick}>
+        </button>
     )
 }
 
